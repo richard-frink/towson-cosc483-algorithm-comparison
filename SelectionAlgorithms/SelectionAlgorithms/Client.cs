a9fb5621eed65a3652c0e864967e37bb3792aa6e
Ricky <Ricky@DESKTOP-RERPI7H>
2016-11-29 13:45:00 -0500

    Laid out methods and overall basic client structure.

--- SelectionAlgorithms/SelectionAlgorithms/Client.cs
+++ SelectionAlgorithms/SelectionAlgorithms/Client.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,7 +14,41 @@ namespace SelectionAlgorithms
             //will use this to easily generate new array objects of various sizes with random elements
             ArrayFactory arrayCreator = new ArrayFactory();
 
+            //Stopwatch keeps track of times
+            Stopwatch stopwatch = new Stopwatch();
+            long lastTestElapsed = -1;
 
+
+            //method is newArray(size of array, range of array (0-integer input) )
+            int[] test1 = arrayCreator.newArray(10000, 10000);
+
+            //here will be tons of running of all of the algorithms and outputs of times and such
+
+
+            //example os stopwatch usage
+            stopwatch.Start();
+            DetSelection(test1, 117);
+            stopwatch.Stop();
+            lastTestElapsed = stopwatch.ElapsedMilliseconds;
+
+        }
+
+        //This is the Deterministic version of Selection sort
+        public static void DetSelection(int[] array, int key)
+        {
+
+        }
+
+        //This is the Randomized version of Selection sort
+        public static void RandSelection(int[] array, int key)
+        {
+
+        }
+
+        //This is where we use randomized quicksort to do the sorting and then select the element we need
+        public static void RandQuickSelect(int[] array, int key)
+        {
+            
         }
     }
 }
